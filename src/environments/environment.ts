22e0df12f85cf055d50dacd863d71483463ba503
Zensi77 <juanmaespi7@gmail.com>
2025-05-27 22:59:37 +0200

    Corrige URLs en el archivo de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
 
-  task_base_url: 'https://https://notion-clone-backend-iqcb.onrender.com/tasks',
-  user_base_url: 'https://https://notion-clone-backend-iqcb.onrender.com/users',
-  auth_base_url: 'https://https://notion-clone-backend-iqcb.onrender.com/auth',
-  shared_base_url:
-    'https://https://notion-clone-backend-iqcb.onrender.com/shared',
+  task_base_url: 'https://notion-clone-backend-iqcb.onrender.com/tasks',
+  user_base_url: 'https://notion-clone-backend-iqcb.onrender.com/users',
+  auth_base_url: 'https://notion-clone-backend-iqcb.onrender.com/auth',
+  shared_base_url: 'https://notion-clone-backend-iqcb.onrender.com/shared',
 };
